44622e72bf044637f25b98502c8b51431fd5c7a5
ploymel <ploy_mel@hotmail.com>
2018-12-25 13:38:35 +0700

    add home page frontend

---
+++ grapebanban/resources/views/home/index.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<div class="container">
+    <div class="row">
+    @foreach ($products as $product)
+        <div class="col col-md-4 col-sm-12 col-12">
+            <div class="card text-center">
+                <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product['name'] }}</h5>
+                    <p class="card-text">ราคา {{ $product['cost'] }} บาท/{{ $product['unit'] }}</p>
+                    @if ($product['numberInStock'] == 0)
+                        <p class="card-text text-danger">สินค้าหมดชั่วคราว</p>
+                        <button type="button" class="btn btn-primary" disabled><i class="fas fa-shopping-cart"></i> หยิบใส่ตะกร้า</button>
+                    @else
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-danger"><i class="fas fa-minus"></i></button>
+                            <input type="text" class="form-control" value="1" />
+                            <button type="button" class="btn btn-success"><i class="fas fa-plus"></i></button>
+                        </div>
+                        <button type="button" class="btn btn-primary"><i class="fas fa-shopping-cart"></i> หยิบใส่ตะกร้า</button>
+                    @endif
+                </div>
+            </div>
+        </div>
+    @endforeach
+    </div>
+    <div class="container">
+        <!--check feedback -->
+        @if (count($feedbacks) === 0)
+            ยังไม่มีความคิดเห็น
+        @else
+            @foreach ($feedbacks as $feedback)
+                <div class="row">
+                    <div class="col-md-6 offset-md-3">
+                        <div class="row">
+                            {{ $feedback['orderNumber'] }}
+                        </div>
+                        <div class="row">
+                            {{ $feedback['userName'] }}
+                        </div>
+                        <div class="row">
+                            {{ $feedback['reviews'] }}
+                        </div>
+                        <div class="row">
+                            <span>
+                                @for ($i = 0; $i < count($feedback['stars']); $i++)
+                                    <i class="fas fa-star" style="color: yellow;"></i>
+                                @endfor
+                                @for ($i = 0; $i < 5 - count($feedback['stars']); $i++)
+                                    <i class="fas fa-star"></i>
+                                @endfor
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endif
+    </div>
+</div>
+@endsection
--- grapebanban/resources/views/layouts/app.blade.php
+++ grapebanban/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
         @yield('css')
 
     </head>
-    <body bgcolor="#CC99EE">
+    <body bgColor="#CC99EE">
         <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
         <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
         <nav class="my-2 my-md-0 mr-md-3">
