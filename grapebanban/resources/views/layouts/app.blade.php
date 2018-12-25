ed42b8c775b28980b641269981fae0bfd72fcd64
iitim <chayanin2007@gmail.com>
2018-12-25 11:48:01 +0700

    add css,js to blade

--- grapebanban/resources/views/layouts/app.blade.php
+++ grapebanban/resources/views/layouts/app.blade.php
@@ -1,6 +1,12 @@
-<html>
+<html lang="en">
     <head>
+        <meta charset="utf-8">
         <title>App Name - @yield('title')</title>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        @yield('css')
+
     </head>
     <body>
         @section('sidebar')
@@ -10,5 +16,18 @@
         <div class="container">
             @yield('content')
         </div>
+
+        <footer class="text-muted">
+            <div class="container">
+                <p class="float-right">
+                <a href="#">Back to top</a>
+                </p>
+                <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+                <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+            </div>
+        </footer>
+
+        <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+        @yield('js')
     </body>
 </html>
